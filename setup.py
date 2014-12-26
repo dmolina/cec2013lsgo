56ab15a914802c63bae632440e232282c684ee33
"dmolina ext:(%22) <daniel.molina@uca.es>
2014-12-26 21:23:17 +0100

    Improved pep8 warnings

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 import sys
 
-#cython = Extension('ea.cbenchmarks',
+# cython = Extension('ea.cbenchmarks',
 #    sources = ['ea/cbenchmarks.pyx'],
 ##    include_dirs = ['include/']
 #)
@@ -14,16 +14,13 @@ sourcefiles = ['cec2013lsgo/cec2013.pyx']
 sourcefiles += ['cec2013lsgo/eval_func.cpp', 'cec2013lsgo/Benchmarks.cpp']
 
 for i in range(1, 16):
-    sourcefiles += ['cec2013lsgo/F%d.cpp' %i] 
-
+    sourcefiles += ['cec2013lsgo/F%d.cpp' % i]
 
 cec2013lsgo = Extension("cec2013lsgo.cec2013",
-              sourcefiles,
-	      language = "c++",
-              extra_compile_args=["-std=c++0x"],
-#	      extra_compile_args=["-Wall -pedantic -std=c++0x"],
-              libraries=["m"]) # Unix-like specific
-
+                        sourcefiles,
+                        language="c++",
+                        extra_compile_args=["-std=c++0x"],
+                        libraries=["m"])  # Unix-like specific
 
 
 class PyTest(TestCommand):
@@ -41,27 +38,31 @@ class PyTest(TestCommand):
     def run_tests(self):
         #import here, cause outside the eggs aren't loaded
         import pytest
+
         errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
+
 setup(
-	name='cec2013lsgo',
-        version='0.1',
-        author='Daniel Molina',
-        author_email='daniel.molina@uca.es',
-        maintainer='Daniel Molina',
-        description='Package for benchmark for the Real Large Scale Global Optimization session on IEEE Congress on Evolutionary Computation CEC\'2013',
-        long_description=open('README.rst').read(),
-        license='GPL V3',
-        url = 'https://github.com/dmolina/cec2013lsgo',
-        packages=['cec2013lsgo'],
-        install_requires=['cython', 'numpy'],
-        ext_modules=cythonize(cec2013lsgo),
-	package_data={'cec2013lsgo': ['cdatafiles/*.txt']},
-	tests_require=['pytest'],
-	cmdclass={'build_ext': build_ext, 'test': PyTest},
-        classifiers = [
-        'Development Status :: 5 - Production/Stable',                     
+    name='cec2013lsgo',
+    version='0.1',
+    author='Daniel Molina',
+    author_email='daniel.molina@uca.es',
+    maintainer='Daniel Molina',
+    description='Package for benchmark for the Real \
+    Large Scale Global Optimization session on IEEE \
+    Congress on Evolutionary Computation CEC\'2013',
+    long_description=open('README.rst').read(),
+    license='GPL V3',
+    url='https://github.com/dmolina/cec2013lsgo',
+    packages=['cec2013lsgo'],
+    install_requires=['cython', 'numpy'],
+    ext_modules=cythonize(cec2013lsgo),
+    package_data={'cec2013lsgo': ['cdatafiles/*.txt']},
+    tests_require=['pytest'],
+    cmdclass={'build_ext': build_ext, 'test': PyTest},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        ]
+    ]
 )
--- tests/test_bench.py
+++ tests/test_bench.py
@@ -1,67 +1,73 @@
 from cec2013lsgo import cec2013
 import pytest
 import numpy as np
-import time
+
 
 @pytest.fixture
 def bench():
-    return cec2013.Benchmark();
+    return cec2013.Benchmark()
+
 
 @pytest.fixture
 def sol():
     return np.zeros(1000)
 
+
 def test_info(bench):
-    """ 
+    """
     Test the information about each function
     """
-    expected_upper = [100, 100, 100, 100, 100, 32, 100, 100, 5, 32, 100, 100, 100, 100, 100]
+    expected_upper = [100, 100, 100, 100, 100,
+                      32, 100, 100, 5, 32, 100, 100, 100, 100, 100]
     num_funs = bench.get_num_functions()
     assert num_funs == 15
 
-    for i in range(1,num_funs+1):
+    for i in range(1, num_funs + 1):
         info = bench.get_info(i)
-        assert info['lower'] == info['upper']*-1
-        assert info['upper'] == expected_upper[i-1]
+        assert info['lower'] == info['upper'] * -1
+        assert info['upper'] == expected_upper[i - 1]
         assert info['threshold'] == 0
         assert info['best'] == 0
-	assert info['dimension'] == 1000
+        assert info['dimension'] == 1000
+
 
 def test_nonan(bench, sol):
     """
     Check that for all solutions is not nan
     """
-    for fun_id in range(1, bench.get_num_functions()+1):
+    for fun_id in range(1, bench.get_num_functions() + 1):
         fn = bench.get_function(fun_id)
         assert not np.isnan(fn(sol))
+
 
 def test_values(bench, sol):
     """
     Check the results for zero for each function
     """
     values_expected = [
- 2.09833896353343505859E+11, 
- 4.76203116166061372496E+04,
- 2.17290025349525492970E+01,
- 1.07955147656065953125E+14,
- 4.84191483329246416688E+07,
- 1.07773246530947787687E+06,
- 9.93826981321072625000E+14,
- 5.72227150187806412800E+18,
- 6.00160320250193595886E+09,
- 9.81154816486999392509E+07,
- 1.04485201647212016000E+17,
- 1.71135423694972143555E+12,
- 8.27380048985966720000E+16,
- 4.40797968120962457600E+18,
- 2.39389233661550150000E+15]
+        2.09833896353343505859E+11,
+        4.76203116166061372496E+04,
+        2.17290025349525492970E+01,
+        1.07955147656065953125E+14,
+        4.84191483329246416688E+07,
+        1.07773246530947787687E+06,
+        9.93826981321072625000E+14,
+        5.72227150187806412800E+18,
+        6.00160320250193595886E+09,
+        9.81154816486999392509E+07,
+        1.04485201647212016000E+17,
+        1.71135423694972143555E+12,
+        8.27380048985966720000E+16,
+        4.40797968120962457600E+18,
+        2.39389233661550150000E+15]
 
     for fun_id in range(1, 16):
         fn = bench.get_function(fun_id)
-        assert fn(sol) == values_expected[fun_id-1]
+        assert fn(sol) == values_expected[fun_id - 1]
 
-#def test_many_functions(bench,sol):
-#    print ""
+# def test_many_functions(bench,sol):
+# print ""
+#    import time
 #
 #    startglobal = time.time()
 #
